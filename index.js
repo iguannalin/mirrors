813eece94c32cf539c336d30fac5ee4e9d0b9aac
seasons986 <annalin12345@gmail.com>
2023-05-31 15:16:35 +0800

    done

---
+++ index.js
@@ -0,0 +1,47 @@
+const subjects = ["she", "he", "it", "they", "they"];
+const objects = ["her", "him", "it", "them", "them"];
+
+window.addEventListener("load", () => {
+  let subject = subjects; // copy of array for manipulating
+  let object = objects;
+  let clicked = 0;
+  let first = subject[Math.floor(Math.random() * subject.length)]; // initialize first randomly
+  let second = 0;
+  let x = 0;
+  let y = 0;
+  const spacing = window.innerHeight / 15;
+  function showLine() {
+    clicked++;
+    let r = Math.floor(Math.random() * subject.length);
+
+    second = object[r];
+    object = objects.filter((o) => o != second);
+    console.log(subject);
+
+    if (clicked % 2 == 1) { // alternate display text between left and right
+      x = "left";
+      y += spacing;
+    } else {
+      x = "right";
+      y += spacing;
+    }
+
+    let kid = document.createElement("p");
+    kid.innerText = `${first} ${first == "they" ? "were" : "was"} looking at ${second}`;
+    kid.style.textAlign = x;
+    kid.style.top = `${y}px`;
+    document.body.appendChild(kid);
+    kid.scrollIntoView();
+
+    first = subject[r];
+    subject = subjects.filter((s) => s != first);
+
+    if (subject.length < 1) subject = subjects;
+    if (object.length < 1) object = objects;
+  }
+  
+  showLine();
+  document.addEventListener("click", () => {
+    showLine();
+  });
+});
